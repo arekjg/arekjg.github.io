67a0f8e0531a969706d9d718a109284a31c3a23c
arekjg <arekjg@gmail.com>
2023-12-21 22:18:56 +0100

    changed styles of navigation menu on small screens, added descriptions to projects

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,32 @@
 import React from "react";
+import { StackIcon } from "./Icons";
 
 const About = () => {
   return (
     <div id="about">
-      I'm a self-taught programmer, continually seeking new challenges to fuel
-      my growth.
-      <br />
-      I have a background in various engineering fields, but in early 2023 I
-      made a career change and started my first programming job.
-      <br />
-      Tech stack I have been working with lately:
-      <ul>
-        <li>.NET</li>
-        <li>MS SQL</li>
-        <li>DevExpress</li>
-        <li>React</li>
-      </ul>
+      <p>
+        I'm a self-taught programmer, continually seeking new challenges to fuel
+        my growth.
+      </p>
+      <p>
+        I have a background in various engineering fields, but in early 2023 I
+        made a career change and started my first programming job.
+      </p>
+      <p className="p-center">Stack I use on a daily basis:</p>
+      <p className="p-center">
+        <StackIcon name={"net"} />
+        <StackIcon name={"mssql"} />
+      </p>
+
+      <p className="p-center">Stack I have some experience with:</p>
+
+      <p className="p-center">
+        <StackIcon name={"html"} />
+        <StackIcon name={"css"} />
+        <StackIcon name={"js"} />
+        <StackIcon name={"react"} />
+        <StackIcon name={"python"} />
+      </p>
     </div>
   );
 };
